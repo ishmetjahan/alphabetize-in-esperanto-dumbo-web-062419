2da249f60a7195994b0d82af1464b0ccc028664b
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 09:21:54 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,9 @@
 def alphabetize(arr)
   esp_alp = ["a", "b", "c", "ĉ", "d", "e", "f", "g", 
-  "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m"," n"," o"," p", r, s,
-  ŝ, t, u, ŭ, v, z]
+  "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m"," n"," o","
+  p", "r"," s"," ŝ", "t", "u"," ŭ", "v", "z"]
+  
+  arr.sort_by do |ele|
+    ele.split(" ").collect do |letter|
+      esp_alp.index 
 end
